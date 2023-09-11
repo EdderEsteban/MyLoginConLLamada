689af4e0c6caaab8b5cd1cd9633ce4658e74d3d9
EdderEsteban <edder709@gmail.com>
2023-09-11 00:35:01 -0300

    Commit Login Listo

--- app/src/main/java/com/ddrssoft/myloginconllamada/MainActivity.java
+++ app/src/main/java/com/ddrssoft/myloginconllamada/MainActivity.java
@@ -1,14 +1,32 @@
 package com.ddrssoft.myloginconllamada;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.ddrssoft.myloginconllamada.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivityViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vm.login(binding.etUsuario.getText().toString(), binding.etPassword.getText().toString());
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/ddrssoft/myloginconllamada/MainActivityViewModel.java
@@ -0,0 +1,33 @@
+package com.ddrssoft.myloginconllamada;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private Context context;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        context = application.getApplicationContext();
+    }
+
+    public void login(String usuario, String password) {
+
+        if (usuario.equals("correo@mail.com") && password.equals("123")) {
+            //Intent intent = new Intent(context, MenuLogin.class);
+            //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //context.startActivity(intent);
+            Toast.makeText(context, "Ingreso al Login", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(context, "Los datos  ingresados son incorrectos", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
