7cb23b7e5b2aed21fe1b24541ccc41b9d6657ec7
EdderEsteban <edder709@gmail.com>
2023-09-11 03:04:58 -0300

    Commit de las 3am

--- app/src/main/java/com/ddrssoft/myloginconllamada/ui/home/HomeViewModel.java
+++ app/src/main/java/com/ddrssoft/myloginconllamada/ui/home/HomeViewModel.java
@@ -1,12 +1,17 @@
 package com.ddrssoft.myloginconllamada.ui.home;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -22,12 +27,18 @@ public class HomeViewModel extends AndroidViewModel {
 
     public Intent telefono(String numero){
         try {
-            Intent intent = new Intent(Intent.ACTION_CALL);
-            intent.setData(Uri.parse(numero));
-            return intent;
-        }catch (NullPointerException npe){
-            Toast.makeText(context, "Ingrese un Numero", Toast.LENGTH_SHORT).show();
-        }
+            int permiso = ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE);
+            if(permiso != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(context, "No tiene Permisos de Llamada", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CALL_PHONE}, 255);
+            }else{
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + numero));
+                return intent;
+            }
+            }catch (NullPointerException npe){
+                Toast.makeText(context, "Ingrese un Numero", Toast.LENGTH_SHORT).show();
+            }
         return null;
     }
 }
